badb1110d27e43c2f2ef046872f61e6fa589f072
ysk <yu.free.t@gmail.com>
2019-02-11 14:54:55 +0900

    quotation update

---
+++ quotation/jscode.js
@@ -0,0 +1,4 @@
+var br = "<br>";
+var qtstr = '"おはよう"は英語で"Good morning"です';
+var escstr = "\"ありがとう\"は英語で\"Thank you\"です";
+document.body.innerHTML = qtstr + br + escstr;
---
+++ quotation/tscode.ts
@@ -0,0 +1,5 @@
+const br = "<br>";
+let qtstr = '"おはよう"は英語で"Good morning"です';
+let escstr = "\"ありがとう\"は英語で\"Thank you\"です";
+
+document.body.innerHTML  =qtstr+br+escstr;
