991d75664079962d33b5a0c9e6ee58a4885f3462
ysk <yu.free.t@gmail.com>
2019-02-11 14:05:36 +0900

    variables update

---
+++ variale/jscode.js
@@ -0,0 +1,4 @@
+var myStr = "3の2乗は";
+var myUnkownStr;
+myUnkownStr = "大きいです.";
+document.body.innerHTML = myStr + myUnkownStr;
---
+++ variale/tscode.ts
@@ -0,0 +1,5 @@
+let myStr="3の2乗は";
+let myUnkownStr:string;
+myUnkownStr="大きいです.";
+
+document.body.innerHTML=myStr+myUnkownStr;
